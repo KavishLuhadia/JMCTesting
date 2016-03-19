635e58aac609aa59670eacd0cfcddf0f7adbc17a
Kavish <Kavish.luhadia@gmail.com>
2016-03-20 01:30:35 +0530

    1

--- SpringWsWorkspace/src/main/java/com/kavish/common/DrawingApp.java
+++ SpringWsWorkspace/src/main/java/com/kavish/common/DrawingApp.java
@@ -19,6 +19,7 @@ public class DrawingApp {
 	 System.out.println("Again making changes");
 
 System.out.println("FETCH 123456");
+System.out.println("PUSHED TO MASTER 1");
 	 System.out.println("Making changes in branch2");
 
 
