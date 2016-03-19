608a7d94e352997a897f7e8981d0e483e021568f
Kavish <Kavish.luhadia@gmail.com>
2016-03-20 01:34:08 +0530

    12

--- SpringWsWorkspace/src/main/java/com/kavish/common/DrawingApp.java
+++ SpringWsWorkspace/src/main/java/com/kavish/common/DrawingApp.java
@@ -18,7 +18,7 @@ public class DrawingApp {
 	 System.out.println("In new branch lets c");
 	 System.out.println("Again making changes");
 
-System.out.println("FETCH 123456");
+System.out.println("FETCH 123456 FOR BR1 --SRY LATE");
 	 System.out.println("Making changes in branch2");
 
 
