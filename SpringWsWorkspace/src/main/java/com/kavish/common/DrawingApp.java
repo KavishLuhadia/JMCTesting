d9d1da55d2012b16666a89fc8a4b3cb4b201e222
Kavish <Kavish.luhadia@gmail.com>
2016-03-20 00:50:27 +0530

    committted

--- SpringWsWorkspace/src/main/java/com/kavish/common/DrawingApp.java
+++ SpringWsWorkspace/src/main/java/com/kavish/common/DrawingApp.java
@@ -8,10 +8,19 @@ public class DrawingApp {
 	 ApplicationContext context = new ClassPathXmlApplicationContext(
 				"Spring-Module.xml"); 
 	 System.out.println("Implementing spring core");
+<<<<<<< Upstream, based on NewBranch1
 
 	 System.out.println("In new branch lets c");
 
 
+=======
+<<<<<<< Upstream, based on NewBranch2
+	 System.out.println("Making changes in branch2");
+=======
+	 System.out.println("In new branch lets c");
+	 System.out.println("Again making changes");
+>>>>>>> c0555d9 Systemout for new branch1 letsc
+>>>>>>> b95dbe8 committted 
 	 Shape shape = (Shape)context.getBean("circle");
 	 System.out.println("changes in new bracnh2 for checking rebase");
 	 System.out.println("This time check rebase");
